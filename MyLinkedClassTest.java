11ca039ebad50066c8d6072a3f069ac662d792d9
joonoaspdosa <36099341+joonoaspdosa@users.noreply.github.com>
2019-03-18 08:59:34 +0900

    inner class

---
+++ MyLinkedClassTest.java
@@ -0,0 +1,12 @@
+public class MyLinkedClassTest {
+	public static void main(String[] args) {
+		MyLinkedClass myList = new MyLinkedClass();
+		myList.print();
+		
+		myList.add("JAVA");
+		myList.add("INNER");
+		myList.add("CLASS");
+		myList.print();		
+	}
+
+}
